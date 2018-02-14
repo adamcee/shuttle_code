6e7af39c83a3abe69159f0a238a936553a0c7b59
adamcee <adam.cahan@gmail.com>
2018-02-13 18:15:06 -0800

    add shebang to postgres_dev_setup.sh

--- postgres_dev_setup.sh
+++ postgres_dev_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 psql -c "CREATE ROLE shuttle_code_dev WITH PASSWORD 'ShuttleC0de!';"
 psql -c "CREATE DATABASE shuttle_code_dev;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE shuttle_code_dev TO shuttle_code_dev;"
