241a305d137240efe42ae5bfc166764aa67a8d8e
adamcee <adam.cahan@gmail.com>
2018-02-14 16:42:58 -0800

    - use dotenv with config.js for sequelize orm. 	new file:   src/config/config.js - Add .env file for default local dev DB secrets. Since its local dev, for our purposes right now this is safe. 	new file:   .env

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+'use strict';
+const dotenv = require('dotenv').config();
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        dialect: "postgres"
+    },
+}
