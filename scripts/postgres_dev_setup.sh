8713e1329288fcdbcae069cede26af19c11c5344
Adam Cahan <adam.cahan@gmail.com>
2018-02-15 21:11:22 -0800

    Delete postgres_dev_setup.sh

--- scripts/postgres_dev_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-psql -c "CREATE ROLE shuttle_code_dev WITH PASSWORD 'ShuttleC0de!';"
-psql -c "CREATE DATABASE shuttle_code_dev;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE shuttle_code_dev TO shuttle_code_dev;"
-psql -c "ALTER ROLE shuttle_code_dev WITH LOGIN;"
