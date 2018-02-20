6a703bb1c2c554c43f8bcc4bb524be4f6104e364
adamcee <adam.cahan@gmail.com>
2018-02-19 21:43:36 -0800

    - remove files and dirs for the ruby-based standalone_migrations tool 	deleted:    Gemfile 	deleted:    Gemfile.lock 	deleted:    Rakefile 	deleted:    db/config.yml 	deleted:    db/migrate/20180217023751_employers.rb 	deleted:    db/schema.rb

--- Rakefile
+++
@@ -1,3 +0,0 @@
-# From: https://github.com/thuss/standalone-migrations/blob/master/example/Rakefile
-require 'standalone_migrations'
-StandaloneMigrations::Tasks.load_tasks
--- db/migrate/20180217023751_employers.rb
+++
@@ -1,8 +0,0 @@
-class Employers < ActiveRecord::Migration[5.1]
-  def change
-      create_table :employers do |t|
-          t.string :name, null: false
-          t.timestamps
-      end
-  end
-end
